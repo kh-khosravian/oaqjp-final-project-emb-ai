5e4366e6410d407b852d2ea48609599306decacc
kh-khosravian <khosravian@outlook.com>
2026-06-30 22:38:58 -0700

    EmotionDetection

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    if not isinstance(text_to_analyze,str):
+        raise TypeError("'text_to_analyze' should be string")
+    
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url,json = payload, headers = headers)
+
+    if response.status_code != 200:
+        return {}
+    
+    emotions = json.loads(response.text)['emotionPredictions'][0]['emotion']
+    return {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': max(emotions, key=emotions.get)
+    }
